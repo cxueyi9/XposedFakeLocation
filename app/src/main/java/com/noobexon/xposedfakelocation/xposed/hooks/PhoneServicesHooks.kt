1b7f3cbdb0fee0a0add7ecdd4d9c44ce588f657a
cxueyi9 <119404752+cxueyi9@users.noreply.github.com>
2026-08-24 18:30:32 +0800

    Update PhoneServicesHooks.kt

--- app/src/main/java/com/noobexon/xposedfakelocation/xposed/hooks/PhoneServicesHooks.kt
+++ app/src/main/java/com/noobexon/xposedfakelocation/xposed/hooks/PhoneServicesHooks.kt
@@ -105,7 +105,7 @@ class PhoneServicesHooks(
     private fun shouldSpoofArgs(args: List<Any?>?): Boolean {
         if (PreferencesUtil.getIsPlaying() != true) return false
         val uid = Binder.getCallingUid()
-        val userId = android.os.UserHandle.getUserId(uid)
+        val userId = android.os.UserHandle.of(uid).identifier
         return args?.asSequence()
             ?.mapNotNull(::extractPackageName)
             ?.any { PreferencesUtil.isPackageTargeted(it, userId) } == true
